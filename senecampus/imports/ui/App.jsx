c1782b1fa67828e474ca496a15c1bd05c7816fcb
estebandalelr <estebandalelr@gmail.com>
2017-10-24 18:42:04 -0500

    Created imports folder which contains structure for project

---
+++ senecampus/imports/ui/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>HELLo</h1>
+      </div>
+    );
+  }
+
+}
+
+export default App;
