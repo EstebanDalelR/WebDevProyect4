a82f4d80db14e76dd6787c91d5a65482604764ea
estebandalelr <estebandalelr@gmail.com>
2017-10-24 20:06:34 -0500

    Added parent element Reports

---
+++ senecampus/imports/ui/Reports/Reports.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Reports extends Component {
+
+  render() {
+    return (
+      <div>
+        <h3>Reports go here</h3>
+      </div>
+    );
+  }
+
+}
+
+export default Reports;
