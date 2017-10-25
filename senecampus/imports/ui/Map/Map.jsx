3c5300ec81d23f05e22ace28110c66999ab1d01c
estebandalelr <estebandalelr@gmail.com>
2017-10-25 15:01:08 -0500

    Got the canvas to show the map

--- senecampus/imports/ui/Map/Map.jsx
+++ senecampus/imports/ui/Map/Map.jsx
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
-
-// import uniandesMap from '../../public/uniandesMap.png';
-
+import ReactDOM from 'react-dom';
 class Map extends Component {
 
+  mapOnCanvas(){
+    window.onload = function() {
+      var c=document.getElementById("myCanvas");
+      const ctx=c.getContext("2d");
+      var img=document.getElementById("uniandesMap");
+      ctx.drawImage(img,0,0);
+    };
+  }
   render() {
+
     return (
       <div>
         <img
-         id = "uniandesMap1"
-         height = "400"
-         width = "400"
-        src = '/uniandesMap.png'
-         alt = "Goofy pic of me" />
-        <canvas id = "drawing"
-          height = "400"
-          width = "400">
-        <p>Canvas not supported</p>
-        </canvas>      </div>
-    );
+          id = "uniandesMap"
+          visibility= "hidden"
+          height = "0"
+          width = "0"
+          src = '/uniandesMap1400.png'
+          alt = "Uniandes Map" />
+          <canvas
+            id = "myCanvas"
+            height = "1500"
+            width = "1570"/>
+          {this.mapOnCanvas()}
+        </div>
+      );
+    }
+
   }
 
-}
-
-export default Map;
+  export default Map;
