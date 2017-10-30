209a06196fbce3655ddf60572c2ab2669ac956d6
estebandalelr <estebandalelr@gmail.com>
2017-10-30 08:54:01 -0500

    Remove unncecesary navbar elements

--- senecampus/imports/ui/Navigation/Navbar.jsx
+++ senecampus/imports/ui/Navigation/Navbar.jsx
@@ -14,7 +14,7 @@ class Navbar extends Component {
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
+              {/* <li className="nav-item active">
                 <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
               </li>
               <li className="nav-item dropdown">
@@ -30,7 +30,7 @@ class Navbar extends Component {
               </li>
               <li className="nav-item">
                 <a className="nav-link disabled" href="#">Disabled</a>
-              </li>
+              </li> */}
               <li className="nav-item acive">
                 <AccountsUIWrapper></AccountsUIWrapper>
               </li>
